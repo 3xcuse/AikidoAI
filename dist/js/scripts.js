c0a01699158a9c1575f418248e6f213501c610c6
3xcuse <68470233+3xcuse@users.noreply.github.com>
2025-07-19 23:37:18 +0200

    extract navbar to separate file

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,26 +4,23 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
+    const placeholder = document.getElementById('navbar-placeholder');
+    const lang = document.documentElement.lang;
+    if (placeholder) {
+        const navFile = lang === 'en' ? 'navbar_en.html' : 'navbar_hu.html';
+        fetch(navFile)
+            .then(response => response.text())
+            .then(html => {
+                placeholder.outerHTML = html;
+                const mainNav = document.getElementById('mainNav');
+                if (mainNav) {
+                    mainNav.classList.add('sticky-top');
+                }
+            });
+    } else {
+        const mainNav = document.getElementById('mainNav');
+        if (mainNav) {
+            mainNav.classList.add('sticky-top');
         }
-        scrollPos = currentTop;
-    });
-})
+    }
+});
